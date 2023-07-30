b4faa67b2a14d23433c43357935b9d4bc0617e37
aaarajuan <aaa.rajuan@gmail.com>
2023-07-30 12:15:10 +0600

    learned how to define variables

---
+++ intro-to-js/variable-name.js
@@ -0,0 +1,9 @@
+var price = 10;
+
+// 1. variable name can not be any keywords
+// 2. in variable there can't be any space
+// 3. variable name can't 've quotation
+// 4. variable can't start with numbers
+// 5. in variable can't use dash (-) but you can use underscore (_) / snake case
+// 6. you can use cat camel case (catCamel) and also you can use pascal case (PascalCase)
+// 7. variable name is case sensitive
