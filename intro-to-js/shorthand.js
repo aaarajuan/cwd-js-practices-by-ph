b5fb56c5b5d9c2edfc9a1aa74fb760b52a1a3dcb
aaarajuan <aaa.rajuan@gmail.com>
2023-07-30 13:15:36 +0600

    learned shorthand in variable operation and created convertion.js

---
+++ intro-to-js/shorthand.js
@@ -0,0 +1,23 @@
+// variable declaration
+var tomatoPrice = 25;
+// variable value update
+tomatoPrice = 30;
+
+// add something to the previous value
+var price1 = 31;
+var price2 = 10;
+
+// set a new value
+// price1 = 35;
+
+// add some value to existing value
+price1 = price1 + 10;
+console.log(price1);
+
+// addition to teh same variable shorthand
+// += means adding some value to existing variable
+price1 += 10;
+
+price2 = price2 - 5;
+// shorthand -=
+price2 -= 5;
