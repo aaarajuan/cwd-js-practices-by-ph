affd4ba92e1e1b923eb9658211e65d9d25556d86
aaarajuan <aaa.rajuan@gmail.com>
2023-07-30 11:50:42 +0600

    added content on variables.js

---
+++ intro-to-js/variable.js
@@ -0,0 +1,16 @@
+// vary + able = variable
+
+// numeric type variable
+var price = 11;
+var age = 21;
+var temperature = 38;
+
+// string type variable
+var person = "sodor uddin";
+var location = "andork killa bandorbon";
+var special = "alia bhat";
+
+// boolean type variable
+var serious = true;
+var marks = true;
+var single = false;
