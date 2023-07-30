36f37a15b4395e947455d4da26af9797bc75518c
aaarajuan <aaa.rajuan@gmail.com>
2023-07-30 12:38:09 +0600

    learned simple  operations in js

---
+++ intro-to-js/operation.js
@@ -0,0 +1,22 @@
+var onionPrice = 20;
+var eggPrice = 10;
+// addition & substraction
+var totalPrice = onionPrice + eggPrice;
+var priceDiffernce = onionPrice - eggPrice;
+// console.log(onionPrice);
+// console.log(eggPrice);
+// console.log(onionPrice + eggPrice);
+// console.log(totalPrice);
+// console.log(priceDiffernce);
+
+// multiplication
+// var orangePrice = 20;
+// var quantity = 7;
+// var totalCost = orangePrice * quantity;
+// console.log(totalCost);
+
+// division
+var money = 500;
+var player = 10
+var eachPlayer = money / player;
+console.log(eachPlayer);
