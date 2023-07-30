b03d7ab2130e252434f675dd076d3d3d98d2c140
aaarajuan <aaa.rajuan@gmail.com>
2023-07-30 11:30:47 +0600

    added math needed to learn for programming

---
+++ intro-to-js/math-needed-for-programming.js
@@ -0,0 +1,19 @@
+/*** 
+ * 1. numbers ==> 55. 898, -32
+ * 2. integer: 2, 5
+ * 3. float: decimal: 2.5, 1357, 32.69
+ * 4. operation: addition, substraction, multiplication, division, remainer
+ * 5. percent,
+ * 6. average
+ * 7. comparison: >, <, ==, !=, >=, <=
+ * 8. okik: 2 kilo apple and 5 kilo mango:
+ * 9. 45, 25, 68, 129: max, min, avg, sum
+ * 10. series
+ * ::Extra::
+ * 11. jor, bijor
+ * 12. prime number / molik songkha
+ * 13. permutaion
+ * 14. area
+ * :::always:::
+ * jodi lage sikhe nite hobe
+ *  ***/
