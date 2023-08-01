b7d5b9af9b7df2a7068ba119cb0e361ea3a3b68c
aaarajuan <aaa.rajuan@gmail.com>
2023-08-01 18:45:19 +0600

    added array.js

---
+++ js-fundamental/array.js
@@ -0,0 +1,7 @@
+var friendAge = [11, 21, 45, 17, 14, 105, 6];
+// array elements/upadan always starts with 0
+var picnicFee = [500, 2000, 4000, 150];
+var nayikas = ['mahi', 'opu', 'purnima', 'ariana'];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+console.log(vowels.length);
