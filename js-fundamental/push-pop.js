d816130d91aab7a38fd46cb4b99635699a518063
aaarajuan <aaa.rajuan@gmail.com>
2023-08-01 20:21:10 +0600

    added push-pop.js

---
+++ js-fundamental/push-pop.js
@@ -0,0 +1,8 @@
+var numbers = [78, 45, 98, 45];
+// 1. adding element in array: use .push() to add element in the last
+// numbers.push(63);
+console.log(numbers);
+
+// use pop to get last element
+numbers.pop(); // remove or ber kore fela element ke .pop() diye
+console.log(numbers);
