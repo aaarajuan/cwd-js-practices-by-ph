0b82ccfb4e6de83243e9c5d0ab66e4411d53ff48
aaarajuan <aaa.rajuan@gmail.com>
2023-08-01 18:59:05 +0600

    learned index of element in array

--- js-fundamental/index.js
+++ js-fundamental/index.js
@@ -1,1 +1,15 @@
-var numbers = [45, 234, 564, 34, 56, 223, 76];
+var numbers = [45, 234, 564, 34, 56, 89, 223, 76];
+
+// 1. get element by index
+var element = numbers[3];
+// console.log(element);
+
+// 2. set element value by index
+numbers[1] = 77;
+numbers[3] = 44;
+// console.log(numbers);
+
+
+// 3. find index of an element
+var positionIndex = numbers.indexOf(89);
+console.log(positionIndex);
