f2f1345a2c2cd4babd76d97b2968d0d5673e63c0
aaarajuan <aaa.rajuan@gmail.com>
2023-08-04 18:46:30 +0600

    solved problem 2

--- js-fundamental/p2.js
+++ js-fundamental/p2.js
@@ -4,6 +4,7 @@ var janeScore = 95;
 var peterScore = 56;
 var jonScore = 40
 
+//my score
 if (myScore >= 80 && myScore < 100) {
     finalGrade = 'A grade';
 }
@@ -19,4 +20,76 @@ else if (myScore >= 40 && myScore < 50) {
 else {
     finalGrade = 'D grade'; 
 }
-console.log(finalGrade);
+console.log('my GPA: ' + finalGrade);
+
+//tom score
+if (tomScore >= 80 && tomScore < 100) {
+    finalGrade = 'A grade';
+}
+else if (tomScore >= 60 && tomScore < 80) {
+    finalGrade = 'B grade';
+}
+else if (tomScore >= 50 && tomScore < 60) {
+    finalGrade = 'C grade';
+}
+else if (tomScore >= 40 && tomScore < 50) {
+    finalGrade = 'D grade';
+}
+else {
+    finalGrade = 'D grade'; 
+}
+console.log('Tom GPA: ' + finalGrade);
+
+//jane score
+if (janeScore >= 80 && janeScore < 100) {
+    finalGrade = 'A grade';
+}
+else if (janeScore >= 60 && janeScore < 80) {
+    finalGrade = 'B grade';
+}
+else if (janeScore >= 50 && janeScore < 60) {
+    finalGrade = 'C grade';
+}
+else if (janeScore >= 40 && janeScore < 50) {
+    finalGrade = 'D grade';
+}
+else {
+    finalGrade = 'D grade'; 
+}
+console.log('Jane GPA: ' + finalGrade);
+
+//peter score
+if (peterScore >= 80 && peterScore < 100) {
+    finalGrade = 'A grade';
+}
+else if (peterScore >= 60 && peterScore < 80) {
+    finalGrade = 'B grade';
+}
+else if (peterScore >= 50 && peterScore < 60) {
+    finalGrade = 'C grade';
+}
+else if (peterScore >= 40 && peterScore < 50) {
+    finalGrade = 'D grade';
+}
+else {
+    finalGrade = 'D grade'; 
+}
+console.log('Peter GPA: ' + finalGrade);
+
+//jon score
+if (peterScore >= 80 && peterScore < 100) {
+    finalGrade = 'A grade';
+}
+else if (peterScore >= 60 && peterScore < 80) {
+    finalGrade = 'B grade';
+}
+else if (peterScore >= 50 && peterScore < 60) {
+    finalGrade = 'C grade';
+}
+else if (peterScore >= 40 && peterScore < 50) {
+    finalGrade = 'D grade';
+}
+else {
+    finalGrade = 'D grade'; 
+}
+console.log('John GPA: ' + finalGrade);
