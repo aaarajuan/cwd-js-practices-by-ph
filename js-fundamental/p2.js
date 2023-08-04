84061964db6e2533fbc05a7dbde435d3d7530e8d
aaarajuan <aaa.rajuan@gmail.com>
2023-08-04 18:40:35 +0600

    solved p2

---
+++ js-fundamental/p2.js
@@ -0,0 +1,22 @@
+var myScore = 85;
+var tomScore = 66;
+var janeScore = 95;
+var peterScore = 56;
+var jonScore = 40
+
+if (myScore >= 80 && myScore < 100) {
+    finalGrade = 'A grade';
+}
+else if (myScore >= 60 && myScore < 80) {
+    finalGrade = 'B grade';
+}
+else if (myScore >= 50 && myScore < 60) {
+    finalGrade = 'C grade';
+}
+else if (myScore >= 40 && myScore < 50) {
+    finalGrade = 'D grade';
+}
+else {
+    finalGrade = 'D grade'; 
+}
+console.log(finalGrade);
