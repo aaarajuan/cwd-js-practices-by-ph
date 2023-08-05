2fda20995bf58b53c7596f688a2220b4e5382d51
aaarajuan <aaa.rajuan@gmail.com>
2023-08-05 15:19:15 +0600

    learned array loop

--- loops/array-loop.js
+++ loops/array-loop.js
@@ -3,9 +3,15 @@
 } */
 
 // target: display every element of an array
-var num = [45, 87, 89, 56, 32, 51, 25];
+/* var num = [45, 87, 89, 56, 32, 51, 25];
 
 for(var i = 0; i < num.length; i++){
     var nums = num[i];
     console.log(nums);
+} */
+
+var items = ['bottle', 'mouse', 'sunglasses', 'pen'];
+for(var i = 0; i < items.length; i++) {
+    var item = items[i];
+    console.log(item);
 }
