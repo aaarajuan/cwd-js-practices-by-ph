54fd7dbb06b36c7966b3d61eccf781e8f0327b8e
aaarajuan <aaa.rajuan@gmail.com>
2023-08-05 14:21:19 +0600

    added numbers.js and created even.js

---
+++ loops/numbers.js
@@ -0,0 +1,6 @@
+// Target: display 1 to 10
+var num = 1;
+while(num <= 10){
+    console.log(num);
+    num++;
+}
--- loops/while-loop.js
+++ loops/while-loop.js
@@ -1,3 +1,11 @@
+/** INFINITE WHILE LOOP
+ * 1. loop variable
+ * 2. codition inside variable
+ * 3. loop body
+ * 4. change the loop variable 
+ */
+
+
 var roastGiven = 0;
 while (roastGiven < 7) {
     console.log('Roast Den, Please!!');
