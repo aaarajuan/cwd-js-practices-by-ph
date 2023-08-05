db7cc9f13600188de2828e01114af90bf5fda463
aaarajuan <aaa.rajuan@gmail.com>
2023-08-05 15:49:18 +0600

    learned reverse while and for loop\

---
+++ loops/reverse.js
@@ -0,0 +1,17 @@
+/* var num = 1;
+while (num <= 10) {
+    console.log(num);
+    num++;
+} */
+
+// while loop reversed way
+var num = 10;
+while (num >= 1) {
+    // console.log(num);
+    num--;
+}
+
+// for loop reversed way
+for (var i = 10; i >= 1; i--) {
+    console.log(i);
+}
