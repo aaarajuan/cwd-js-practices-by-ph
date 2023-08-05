fcf592f9c6a1d781fa31353b8480626b748084d6
aaarajuan <aaa.rajuan@gmail.com>
2023-08-05 14:56:25 +0600

    learned while loop and for loop

---
+++ loops/for-loop.js
@@ -0,0 +1,18 @@
+// while loop:
+// var roastGiven = 0;
+// while(roastGiven < 7){
+//     console.log('Roast Den, Please !!!');
+//     roastGiven++;
+// }
+
+// for loop:
+// for([initialization]; [condition]; [final-expression]){
+//     // statements / for loop body
+// }
+// for(var roastGiven = 0; roastGiven < 7; roastGiven++){
+//     console.log('Roast Den, Please !!!');
+// }
+
+for(var i = 0; i < 7; i++){
+    console.log(i);
+}
