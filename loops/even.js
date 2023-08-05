362a7ffb802d116de0f78f9a3795792f36c5815e
aaarajuan <aaa.rajuan@gmail.com>
2023-08-05 14:27:41 +0600

    learned even and odd / jor and bijor

---
+++ loops/even.js
@@ -0,0 +1,7 @@
+// even numbers: jor songkha
+var num = 0;
+while(num <= 10){
+    console.log(num);
+    // num++;
+    num += 2;
+}
---
+++ loops/odd.js
@@ -0,0 +1,6 @@
+// odd numbers: bijor songkha
+var num = 1;
+while(num < 10){
+    console.log(num)
+    num +=2;
+}
