8f023ce3911af6115a9e7a86d51108174209d640
aaarajuan <aaa.rajuan@gmail.com>
2023-08-06 14:55:57 +0600

    learned function multiple parameters

--- js-function-objects/perameter.js
+++ js-function-objects/perameter.js
@@ -1,5 +1,29 @@
-function bringSingara(money){
+/* function bringSingara(money){
     console.log('mama singara den');
     console.log(money);
 }
-bringSingara(250);
+bringSingara(250); */
+
+// you can think perameter as a kind of variable
+
+function bringSingara(money){
+    console.log('Eto taka disen: ', money);
+    console.log('ai nen singara');
+}
+
+var taka = 300;
+// bringSingara(taka);
+
+function add(num1, num2){
+    console.log('going to add: ', num1, num2);
+}
+
+// add(125, 220);
+
+function sum(a, b, c, d, e){
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log(sum);
+}
+
+sum(15, 98, 56, 5, 9);
