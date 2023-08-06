3dcd8182982237868b43d01dd2af07c2ce32fbf3
aaarajuan <aaa.rajuan@gmail.com>
2023-08-06 14:15:53 +0600

    learnied funtion declaration and call back of function

---
+++ js-function-objects/function.js
@@ -0,0 +1,9 @@
+// function declarations
+function startFan(){
+    console.log('Stand Up');
+    console.log('walk towards the switch');
+    console.log('Press the switch');
+}
+
+// call the function
+startFan();
