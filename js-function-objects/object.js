e07e466c83fc420ed73831dcc5087432bbc18d8d
aaarajuan <aaa.rajuan@gmail.com>
2023-08-06 17:07:21 +0600

    learned js objects

---
+++ js-function-objects/object.js
@@ -0,0 +1,14 @@
+var student = {
+    id: 115,
+    name: 'Solaiman Khan',
+    class: 9,
+    marks: 98
+};
+
+var mobile = {
+    brand: 'Samsung',
+    price: 19000,
+    storage: '64gb',
+    camera: '7MP'
+}
+
