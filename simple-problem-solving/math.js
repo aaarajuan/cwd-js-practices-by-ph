c146a9b14e92fdab3bb0586bf0ea229964375cf7
aaarajuan <aaa.rajuan@gmail.com>
2023-08-08 14:43:36 +0600

    learned javascript Math and methods of maths

---
+++ simple-problem-solving/math.js
@@ -0,0 +1,33 @@
+const result = Math.pow(3, 8);
+// console.log(result);
+
+const num1 = 25;
+const num2 = 45;
+
+const gap = Math.abs(num1 - num2);
+// console.log(gap);
+if(gap < 5){
+    console.log('you guys can be friends');
+}
+else{
+    console.log('you guys stay apart');
+}
+
+// -----------------------
+
+// -----------------------
+const number = 2.4598;
+const fullnumber = Math.round(number);
+// console.log(fullnumber);
+
+const result2 = Math.ceil(2.00001);
+const result3 = Math.floor(456.259);
+// console.log(result2);
+// console.log(result3);
+
+// console.log(Math.random());
+for(let i = 0; i < 20; i++){
+    const random = Math.random()*100;
+    console.log(random);
+}
+
