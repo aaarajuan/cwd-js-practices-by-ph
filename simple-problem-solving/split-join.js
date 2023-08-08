a6c249f2e2175d0111921b9794a8e6419927a258
aaarajuan <aaa.rajuan@gmail.com>
2023-08-08 14:28:07 +0600

    learned javascript string methods

---
+++ simple-problem-solving/split-join.js
@@ -0,0 +1,11 @@
+const lyrics = 'tumi bondhu kala pakhi ami jeno ki. bosonto kale tomai bolte pari ni.';
+const parts = lyrics.split('');
+const sentence = lyrics.split('.');
+const chars = lyrics.split('');
+// console.log(chars);
+
+const partial = lyrics.slice(2, 5);
+// console.log(partial);
+
+const partial2 = lyrics.substring(5, 8);
+console.log(partial2);
