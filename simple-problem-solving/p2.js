5d2178fa788c24c2f1e72517605d6a8b67047af0
aaarajuan <aaa.rajuan@gmail.com>
2023-08-08 15:06:51 +0600

    learned javascript find min and max number using if else, function and math.min and math.max

---
+++ simple-problem-solving/p2.js
@@ -0,0 +1,18 @@
+const jim = 69;
+const dela = 97;
+const cinku = 99;
+
+/* 
+if(jim > dela && jim > cinku){
+    console.log('jim will get the cake');
+}
+else if(dela > jim && dela > cinku){
+    console.log('dela will get the cake');
+}
+else{
+    console.log('cinku will get the cake');
+} 
+*/
+
+const maxNum = Math.max(12, 45, 120);
+console.log(maxNum);
